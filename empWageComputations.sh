c33b98399a900ece0a8b5d8a0d08bc5166bd26ca
unknown <ararky7@gmail.com>
2020-10-05 23:27:06 +0530

    Wages For a Month Program

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -2,17 +2,21 @@
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHr=20;
+numWorkingDays=20;
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
 empCheck=$((RANDOM%3));
-
 case $empCheck in
 	$isFullTime)
 		empHrs=16;;
 	$isPartTime)
 		empHrs=8;;
 	*)
-		empHrs=0;;
+	empHrs=0;;
 esac
 salary=$(($empHrs*$empRatePerHr));
-echo "Salary of Employee is :"$salary;
-
+totalSalary=$(($totalSalary+$salary));
+done
+echo "Salary for a Month is : " $totalSalary;
