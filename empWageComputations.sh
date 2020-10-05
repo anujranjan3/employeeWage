bce99dee970e31c82b2ad0e5029f8a29db7ba544
unknown <ararky7@gmail.com>
2020-10-05 23:15:26 +0530

    Switch Case Part Time Employee Wage Program

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -3,17 +3,16 @@
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
-randomCheck=$((RANDOM%3));
+empCheck=$((RANDOM%3));
 
-if [ $isFullTime -eq $randomCheck ];
-then
-	empHrs=16;
-elif [ $isPartTime -eq $randomCheck ];
-then
-	empHrs=8;
-else
-	empHrs=0;
-fi
-
+case $empCheck in
+	$isFullTime)
+		empHrs=16;;
+	$isPartTime)
+		empHrs=8;;
+	*)
+		empHrs=0;;
+esac
 salary=$(($empHrs*$empRatePerHr));
 echo "Salary of Employee is :"$salary;
+
