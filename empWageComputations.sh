305b1a6e51b51feee13147735a7baaafbef2bfde
unknown <ararky7@gmail.com>
2020-10-06 09:35:41 +0530

    Storing Daily Wage along with Total salary

--- empWageComputations.sh
+++ empWageComputations.sh
@@ -2,8 +2,11 @@
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
 numWorkingDays=0;
 workingHours=0;
+i=0;
 getWorkHrs () {
 	empCheck=$((RANDOM%3));
 case $empCheck in
@@ -16,14 +19,36 @@ case $empCheck in
 esac
 echo $empHrs;
 }
+
+storeDailyWage () {
+	workHr=$workHrs;
+	wagePerDay[((i))]=$(($workHr*$empRatePerHr));
+	totalSalary=$((${wagePerDay[i]}+$totalSalary));
+	(( i++ ));
+	echo $totalSalary;
+}
+
+showWageDetails () {
+	for (( i=0; i<20; i++ ))
+	do
+		echo "Day " $i " Wage is : " ${wagePerDay[i]};
+	done
+	echo "Total Salary is : "$totalSalary;
+}
+
 WageMain () {
 	while [ $numWorkingDays -lt 20  ]
 do
 	workHrs=$(getWorkHrs);
 	workingHours=$(($workingHours+$workHrs));
 	(( numWorkingDays++ ));
+	storeDailyWage;
 done
+
+showWageDetails;
+
 echo "Total Working Hours For the Month is : " $workingHours;
+echo "Total salary(Another Way) :"$(($workingHours*$empRatePerHr));
 }
 
 WageMain;
